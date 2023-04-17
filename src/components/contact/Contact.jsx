0c7a56bfe67ec3b091c3621a6c108e60d6d8db31
raohassan4212 <52700173+raohassan4212@users.noreply.github.com>
2023-04-17 09:59:09 +0500

    Initial commit

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Back from "../common/back/Back"
+import "./contact.css"
+
+const Contact = () => {
+  const map = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d904726.6131739549!2d85.24565535!3d27.65273865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1652535615693!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
+  return (
+    <>
+      <Back title='Contact us' />
+      <h1 style={{textAlign: "center", marginBottom: "60px"}}>Under Construction</h1>
+    </>
+  )
+}
+
+export default Contact
---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Heading from "../../common/heading/Heading";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <>
+      <section className="hero">
+        <div className="container">
+          <div className="row">
+            <Heading
+              subtitle="WELCOME TO Arya"
+              title="Best Education Expertise"
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+              consectetur asperiores, non quibusdam et ea sed inventore, aperiam
+              omnis ullam, 
+            </p>
+            <div className="button">
+              <button className="primary-btn">
+                GET STARTED NOW <i className="fa fa-long-arrow-alt-right"></i>
+              </button>
+              <button>
+                VIEW COURSE <i className="fa fa-long-arrow-alt-right"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="margin"></div>
+    </>
+  );
+};
+
+export default Hero;
